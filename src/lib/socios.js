fa7cf0a6ff0d4ad53d492412350bafae4101709a
Luis Chrestia <chrestia.luis@gmail.com>
2025-06-21 21:37:42 -0300

    wip admin

--- src/lib/socios.js
+++ src/lib/socios.js
@@ -1,5 +1,6 @@
 export const SOCIOS = [
   {
+    "id": 1,
     "nombre": "Lucas",
     "apellido": "Gómez",
     "dni": "32845123",
@@ -7,6 +8,7 @@ export const SOCIOS = [
     "vencimiento": "2025-07-03"
   },
   {
+    "id": 2,
     "nombre": "Camila",
     "apellido": "Fernández",
     "dni": "41672358",
@@ -14,6 +16,7 @@ export const SOCIOS = [
     "vencimiento": "2025-06-22"
   },
   {
+    "id": 3,
     "nombre": "Martín",
     "apellido": "Pérez",
     "dni": "30128754",
@@ -21,6 +24,7 @@ export const SOCIOS = [
     "vencimiento": "2025-06-29"
   },
   {
+    "id": 4,
     "nombre": "Valentina",
     "apellido": "Ríos",
     "dni": "42917384",
@@ -28,6 +32,7 @@ export const SOCIOS = [
     "vencimiento": "2025-06-18"
   },
   {
+    "id": 5,
     "nombre": "Joaquín",
     "apellido": "Martínez",
     "dni": "34891236",
@@ -35,6 +40,7 @@ export const SOCIOS = [
     "vencimiento": "2025-07-05"
   },
   {
+    "id": 6,
     "nombre": "Sofía",
     "apellido": "López",
     "dni": "38871245",
@@ -42,6 +48,7 @@ export const SOCIOS = [
     "vencimiento": "2025-07-01"
   },
   {
+    "id": 7,
     "nombre": "Mateo",
     "apellido": "Gutiérrez",
     "dni": "30984576",
@@ -49,6 +56,7 @@ export const SOCIOS = [
     "vencimiento": "2025-06-21"
   },
   {
+    "id": 8,
     "nombre": "Agustina",
     "apellido": "Molina",
     "dni": "41256347",
@@ -56,6 +64,7 @@ export const SOCIOS = [
     "vencimiento": "2025-07-10"
   },
   {
+    "id": 9,
     "nombre": "Bruno",
     "apellido": "Sánchez",
     "dni": "39281645",
@@ -63,6 +72,7 @@ export const SOCIOS = [
     "vencimiento": "2025-06-25"
   },
   {
+    "id": 10,
     "nombre": "Lucía",
     "apellido": "Navarro",
     "dni": "40692138",
@@ -70,6 +80,7 @@ export const SOCIOS = [
     "vencimiento": "2025-06-20"
   },
   {
+    "id": 11,
     "nombre": "Tomás",
     "apellido": "Ramírez",
     "dni": "32184712",
@@ -77,6 +88,7 @@ export const SOCIOS = [
     "vencimiento": "2025-07-12"
   },
   {
+    "id": 12,
     "nombre": "Martina",
     "apellido": "Ibarra",
     "dni": "43126781",
@@ -84,6 +96,7 @@ export const SOCIOS = [
     "vencimiento": "2025-07-07"
   },
   {
+    "id": 13,
     "nombre": "Emiliano",
     "apellido": "Ortiz",
     "dni": "36892145",
@@ -91,6 +104,7 @@ export const SOCIOS = [
     "vencimiento": "2025-06-19"
   },
   {
+    "id": 14,
     "nombre": "Josefina",
     "apellido": "Ruiz",
     "dni": "40982319",
@@ -98,6 +112,7 @@ export const SOCIOS = [
     "vencimiento": "2025-07-08"
   },
   {
+    "id": 15,
     "nombre": "Franco",
     "apellido": "Cabrera",
     "dni": "39671234",
@@ -105,6 +120,7 @@ export const SOCIOS = [
     "vencimiento": "2025-07-15"
   },
   {
+    "id": 16,
     "nombre": "Milagros",
     "apellido": "Acuña",
     "dni": "41723958",
@@ -112,6 +128,7 @@ export const SOCIOS = [
     "vencimiento": "2025-06-23"
   },
   {
+    "id": 17,
     "nombre": "Santiago",
     "apellido": "Castro",
     "dni": "30547812",
@@ -119,6 +136,7 @@ export const SOCIOS = [
     "vencimiento": "2025-06-27"
   },
   {
+    "id": 18,
     "nombre": "Julieta",
     "apellido": "Silva",
     "dni": "42837491",
@@ -126,6 +144,7 @@ export const SOCIOS = [
     "vencimiento": "2025-07-02"
   },
   {
+    "id": 19,
     "nombre": "Benjamín",
     "apellido": "Moreno",
     "dni": "38927415",
@@ -133,6 +152,7 @@ export const SOCIOS = [
     "vencimiento": "2025-06-24"
   },
   {
+    "id": 20,
     "nombre": "Antonella",
     "apellido": "Herrera",
     "dni": "41389245",
