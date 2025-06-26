828932c17a15015cf6e2807df259f7fc49c4d739
Agustin Chazarreta Cruz <aguschacruz@gmail.com>
2025-06-26 00:12:46 -0300

    Uso de Zustand

--- src/pages/Carrito.jsx
+++ src/pages/Carrito.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
-import { Producto } from './Producto'
+import carritoStore from '../store/storeCarrito'
 
 export const Carrito = () => {
-  
 
-  
+  // const {carrito, eliminarProducto, vaciarCarrito } = carritoStore()
+  // const carrito = carritoStore(state => state.carrito);
+  // const eliminarProducto = carritoStore(state => state.eliminarProducto);
+  // const vaciarCarrito = carritoStore(state => state.vaciarCarrito);
+
   return (
-    <div className='text-amber-50'>Carrito</div>
+   <div></div>
   )
 }
