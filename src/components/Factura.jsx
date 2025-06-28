9d5314bdbb360d0d52cf38cbc914ef318518654b
Agustin Chazarreta Cruz <aguschacruz@gmail.com>
2025-06-28 17:44:11 -0300

    Agregamos Facturacion en el carrito

---
+++ src/components/Factura.jsx
@@ -0,0 +1,46 @@
+import { FaWhatsapp } from "react-icons/fa"
+
+const Factura = ({lista, total, envio, closeFactura, mensaje}) => {
+    return(
+        <div onClick={closeFactura} className="fixed inset-0 flex justify-center items-center backdrop-blur-md z-20 bg-black/50">
+            <div onClick={(e) => e.stopPropagation()} className="flex flex-col items-center gap-1 p-4 bg-white w-full rounded shadow max-w-3xl mx-auto text-black">
+                <h2 className="text-2xl font-bold mb-4">Factura de compra</h2>
+                <table className="w-full border-collapse">
+                    <thead>
+                        <tr className="border-b">
+                            <th className="text-left p-2">Producto:</th>
+                            <th className="text-center p-2">Cantidad:</th>
+                            <th className="text-right p-2">Precio unitario:</th>
+                            <th className="text-right p-2">Subtotal</th>
+                        </tr>
+                    </thead>
+                <tbody>
+                    {lista.map(producto =>(
+                        <tr key={producto.id_producto} className="border-b">
+                            <td className="items-center p-2">{producto.nombre}</td>
+                            <td className="text-center p-2">{producto.cantidad}</td>
+                            <td className="text-right p-2">{producto.precio}</td>
+                            <td className="text-right p-2">{producto.precio * producto.cantidad}</td>
+                        </tr>
+                    ))}
+                    <tr>
+                            <td className="items-center p-2">Costo de envio:</td>
+                            <td className="text-center p-2"></td>
+                            <td className="text-right p-2"></td>
+                            <td className="text-right p-2">{envio}</td>
+                        </tr>
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <td colSpan="3" className="text-right font-bold p-2">Total:</td>
+                        <td className="text-right font-bold p-2">{total + envio}</td>
+                    </tr>
+                </tfoot>
+                </table>
+                <a href={`https://wa.me/3816177174?text=${encodeURIComponent(mensaje)}`} target='_blank' className='flex flex-nowrap gap-1 items-center mx-auto mb-4 p-3 text-center rounded-md text-white bg-green-500 hover:bg-green-600 duration-200 '><FaWhatsapp className="text-xl"/> Comprar por WhatsApp</a>
+            </div>
+        </div>
+    )
+}
+
+export default Factura
